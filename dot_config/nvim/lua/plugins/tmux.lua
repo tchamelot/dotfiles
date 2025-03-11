feb8a458219ea753c5e41f9580606d36cae2b01d
T. Chamelot <chamelot.thomas@gmail.com>
2025-03-11 09:39:45 +0100

    feat: use chezmoi

---
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<A-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<A-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<A-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<A-l>", "<cmd>TmuxNavigateRight<cr>" },
+      { "<A-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}
